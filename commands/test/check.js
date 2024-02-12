59a064ba161181950faee48d9f5de1e29e4a952b
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2024-02-12 22:46:56 +0530

    Update check.js, config.json, message.js, and package.json

--- commands/test/check.js
+++ commands/test/check.js
@@ -9,9 +9,10 @@ module.exports = {
     .setDescription("Get the current count"),
   async execute(interaction) {
     const count = await interaction.client.db.get("count");
+    const leader = await interaction.client.db.get("leader");
     const embed = new EmbedBuilder()
       .setTitle("Current count")
-      .setDescription(`The current count is ${count}`)
+      .setDescription(`The current count is \`${count}\`\nNext number to enter is \`${count+1}\`\nCurrent leader is <@!${leader}>`)
       .setColor("#ff0000");
 
     await interaction.reply({ embeds: [embed] });
--- events/message.js
+++ events/message.js
@@ -1,26 +1,96 @@
-const { Events, ActivityType } = require("discord.js");
+const { Events, PermissionFlagsBits } = require("discord.js");
 
+var safeEval = require("safe-eval");
 var currentCount;
+
+var gemini;
+import("gemini-ai").then((Gemini) => {
+  gemini = new Gemini.default(process.env.gemnikey);
+});
 
 module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
-    if (message.author.id === message.client.user.id) return;
-
-    if (message.channel.id === message.client.config.evalChannel) {
-      console.log(`Received message in DM: ${message.content}`);
-      return message.reply((await eval(message.content)).toString());
-    }
-
-    if (!currentCount) currentCount = await message.client.db.get("count");
-
     const client = message.client;
     const db = client.db;
+
+    if (message.author.id === client.user.id) return;
+    if (message.author.bot) return;
+
+    if (message.channel.id == client.config.aiChannel) {
+      try {
+        message.react("🤔");
+        var response = await gemini.ask(message.content)
+        if (response.length > 2000) {
+          var responses = [];
+          while (response.length > 2000) {
+            responses.push(response.slice(0, 2000));
+            response = response.slice(2000);
+          }
+          responses.push(response);
+          for (var i = 0; i < responses.length; i++) {
+            message.reply(responses[i]);
+          }
+        } else {
+        message.reply(response);
+        }
+        message.reactions.removeAll();
+      } catch {
+        message.reactions.removeAll();
+        message.react("❌");
+      }
+    }
+
+    if (message.content.startsWith(">")) {
+      // check for a webhook named "Anonymous" in the message channel if not then create it in discord.js v14
+      const channel = message.channel;
+      // Fetch webhooks for the channel
+      const webhooks = await channel.fetchWebhooks();
+      let webhook = webhooks.find((w) => w.name === "Anonymous");
+
+      // If the webhook doesn't exist, create it
+      if (!webhook) {
+        webhook = await channel.createWebhook({
+          name: "Anonymous",
+          avatar:
+            "https://www.thetonyrobbinsfoundation.org/wp-content/uploads/2017/09/Cool-avatars-anonymous-avatar.jpg",
+          reason: "Needed a webhook named Anonymous",
+        });
+      }
+
+      webhook.send({
+        content: message.content.slice(1),
+        threadId: message.threadId ? message.threadId : null,
+        files: message.attachments.map((file) => {
+          return file.url;
+        }),
+      });
+      message.delete();
+    }
+
+    if (message.channel.id === client.config.evalChannel) {
+      var evalCnt;
+      try {
+        if (message.member.permissions.has(PermissionFlagsBits.Administrator)) {
+          evalCnt = (await eval(message.content)).toString();
+        } else {
+          evalCnt = safeEval(message.content).toString();
+        }
+      } catch (e) {
+        evalCnt = e.toString();
+      }
+      return message.reply(evalCnt);
+    }
+
+    if (!currentCount) currentCount = await db.get("count");
 
     if (message.channel.id === client.config.countChannel) {
       var newCount = parseInt(message.content);
 
-      if (isNaN(newCount)) return message.delete();
+      if (isNaN(newCount)) {
+        console.log(`[❌] Message is not a number`);
+        return message.delete();
+      }
 
       if (newCount === currentCount + 1) {
         var currentCountingLeader =
@@ -28,6 +98,7 @@ module.exports = {
           (await db.set("leader", message.author.id));
 
         if (currentCountingLeader == message.author.id) {
+          console.log(`[❌] User is already the leader`);
           return message.delete();
         }
 
@@ -43,6 +114,7 @@ module.exports = {
         await db.set("count", newCount);
         console.log(`[📈] Count is now ${newCount}`);
       } else {
+        console.log(`[❌] Wrong Number inputed`);
         message.delete();
       }
     }
